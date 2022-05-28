840f984bf8e49e02e65d113eb1e733d8b15e2757
random.istp <random.istp>
2022-05-28 23:06:31 +0800

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -33,24 +33,28 @@ function App () {
   return (
     <div>
       <div>
-        <a href="https://static.neris-assets.com/images/personality-types/collection.html">Full body avatars are here</a>. To download all avatars, just click ctrl+s to save the website. Please notice we do not own the avatars, this is just a self made tool to browse all the mbti avatars in one go. If you want to make use of them, please contact the author.
+        <a href="https://static.neris-assets.com/images/personality-types/collection.html">Full body avatars are here</a>. To download all avatars, just click ctrl+s to save the website. Please notice we do not own the avatars, this is just a self made tool to browse all the mbti avatars in one go. If you want to make use of them, please contact the author. This fans made site is maintained by random.istp instagram page.
       </div>
-      {Object.entries(mbtis).map(([mbti, name]) => (Object.entries(sexCountMap).map(([sex, counts]) => (
-        <div key={mbti} style={{ display: 'flex', alignContent: 'center' }}>
-          <div>
-            {mbti}
-          </div>
-          <div>
-            {
-              commercials.map(commercial => (
-                counts
-                  .map(n => `https://static.neris-assets.com/images/personality-types/avatars/faces/${mbti}-${name}${commercial}-s3${n}-${sex}.svg`)
-                  .map(src => (<img src={src} key={src} width="100px" onError={({ target }: any) => { target.style.display = 'none' }} />))
-              ))
-            }
-            </div>
-        </div>
-      ))))}
+      <table>
+        <tbody>
+          {Object.entries(mbtis).map(([mbti, name]) => (Object.entries(sexCountMap).map(([sex, counts]) => (
+            <tr key={mbti}>
+              <td>
+                {mbti} {sex}
+              </td>
+              <td>
+                {
+                  commercials.map(commercial => (
+                    counts
+                      .map(n => `https://static.neris-assets.com/images/personality-types/avatars/faces/${mbti}-${name}${commercial}-s3${n}-${sex}.svg`)
+                      .map(src => (<img src={src} key={src} width="100px" onError={({ target }: any) => { target.style.display = 'none' }} />))
+                  ))
+                }
+                </td>
+            </tr>
+          ))))}
+        </tbody>
+      </table>
 
       Hello world
     </div>
